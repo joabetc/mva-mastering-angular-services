fafe5181a60dd86f450ae6efc234dc4a2b6b7f2d
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-09-25 12:32:59 -0300

    Creating a Logging Service

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from "@angular/core";
 
+import { Logger } from "./services/logger";
+
 @Component({
     selector: "main",
     template: "<span>{{message}}</span>",
+    providers: [ Logger ],
 })
 export class AppComponent {
 
     public message: string = "Hello World!";
 
+    constructor(private logger: Logger) {
+        this.logger.log(this.message);
+    }
 }
